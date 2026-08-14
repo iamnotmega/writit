9c51af63ae787b5cb08421f35f60bd46fe992d81
NotMega <iamnotmega@proton.me>
2026-08-14 12:49:33 +0300

    fix: add missing delete_folder to lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            notes::save_note, notes::get_notes, notes::read_note, notes::delete_note, notes::get_folders, notes::create_folder
+            notes::save_note, notes::get_notes, notes::read_note, notes::delete_note, notes::get_folders, notes::create_folder, notes::delete_folder
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
